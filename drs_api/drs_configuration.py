a2bad0c1a21e579a351d17358d7cbb002b0a91ad
CMGG Cloud Team <cloud@cmgg.be>
2022-05-17 19:26:50 +0200

    Extend conf class to handle apiKye/token refresh

--- drs_api/drs_configuration.py
+++ drs_api/drs_configuration.py
@@ -1,41 +1,53 @@
+"""
+    Data Repository Service
+    OpenAPI spec version: 1.2.0
+    Contact: ict@cmgg.be
+"""
+
+from jose import jwt, ExpiredSignatureError
 from drs_api.configuration import Configuration
 from drs_api import LoginApi, ApiClient
 from drs_api.rest import ApiException
-from jose import jwt, ExpiredSignatureError
-# from drs_api import rest
 
 class DrsConfiguration(Configuration):
+    """This class extends the auto generated Configuration class."""
 
     def __init__(self):
+        """Constructor"""
         super().__init__()
         self.api_key_prefix = {"Authorization":"Bearer"}
-        self.refresh_api_key_hook = self.refresh_api_key_hook_function()
+        self.refresh_api_key_hook = self.refresh_api_key_hook_function.__func__
+
 
     def refresh_api_key_hook_function(self):
-        auth_setting = self.auth_settings().get("OAuth2PasswordBearer")
-        identifier = auth_setting['key']
+        """Refresh apiKey hook function
 
+        If no api_key is set OR api_key is set but expired,
+        request a new key/token using the LoginApi class
+        """
+        identifier = "Authorization"
         if self.api_key.get(identifier):
             token = self.api_key.get(identifier)
-            try: 
-                payload = jwt.decode(token, None, options={'verify_signature': False})
+            try:
+                _ = jwt.decode(token, None, options={'verify_signature': False})
                 return
             except ExpiredSignatureError:
                 pass
 
-        
-        # client = rest.RESTClientObject(self)
-        # client.POST(f'{self.host}/token',
-
-        #         )
-        # self.username
-        # self.password
-        # Renew token here
-
         login = LoginApi(ApiClient(self))
         try:
-            login_response = login.login_token_post(username=self.username, password=self.password, grant_type="", scope="", client_id="", client_secret="")
+            login_response = login.login_token_post(
+                username=self.username,
+                password=self.password,
+                grant_type="",
+                scope="",
+                client_id="",
+                client_secret=""
+                )
             assert login_response.token_type.lower() == "bearer"
             self.api_key[identifier] = login_response.access_token
-        except ApiException as e:
-            print("Exception when calling LoginApi->login_token_post: %s\n" % e) 
+        except ApiException as error:
+            raise ApiException(
+                status=0,
+                reason="Failed to refresh apiKey"
+            ) from error
